8b36b46be6dd56c16bf274e22356a73cb01715e0
Oscar Viciana <oscar.viciana@basetis.com>
2018-03-25 10:27:27 +0200

    Testing...

--- utils/http/echo.go
+++ utils/http/echo.go
@@ -7,27 +7,30 @@ import (
 	"github.com/labstack/echo"
 )
 
-type Handler func(map[string]string, map[string]string, map[string]interface{}) interface{}
+type UrlParam map[string]string
+type QueryParams map[string]string
+type GetBody func(interface{}) error
+
+type Handler func(UrlParam, QueryParams, GetBody) interface{}
 
 func WrapperEcho(fn Handler) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		url := make(map[string]string)
-		query := make(map[string]string)
-		body := echo.Map{}
-
 		urlParams := c.ParamNames()
-		queryParams := reflect.ValueOf(c.QueryParams()).MapKeys()
-
 		for _, urlParam := range urlParams {
 			url[urlParam] = c.Param(urlParam)
 		}
 
+		query := make(map[string]string)
+		queryParams := reflect.ValueOf(c.QueryParams()).MapKeys()
 		for _, queryParam := range queryParams {
 			v := queryParam.String()
 			query[v] = c.QueryParam(v)
 		}
 
-		c.Bind(&body)
+		body := func(i interface{}) error {
+			return c.Bind(i)
+		}
 
 		return c.JSON(http.StatusOK, fn(url, query, body))
 	}
